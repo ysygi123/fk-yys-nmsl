c143c75c2f151faa72420a36194f65d712d1b628
ysygi123 <1179364733@qq.com>
2025-10-26 22:57:32 +0800

    1

--- jieping.py
+++ jieping.py
@@ -1,7 +1,7 @@
 import common.myadb
 import cv2
 
-screenshot = common.myadb.adb_screenshot("emulator-5554", (1706, 890, 150, 50))
+screenshot = common.myadb.adb_screenshot("emulator-5554", (1240, 740, 70, 70))
 cv2.imwrite("p1.png", screenshot)
 # screenshot = common.myadb.adb_screenshot("127.0.0.1:16416", (1750, 900, 150, 130))
 # cv2.imwrite("picture/yuhun/tiaozhan.png", screenshot)
