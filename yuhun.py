3d4d73d6bf0256b0bce0f4eb0ea4d0f391304424
ysygi123 <1179364733@qq.com>
2025-11-19 17:55:40 +0800

    御魂

--- yuhun.py
+++ yuhun.py
@@ -14,28 +14,47 @@ def click_tiao_zhan():
     print(f'查看自有匹配结果 {success}')
 
 
+def run_sequence(device):
+    """
+    在单个设备上按顺序运行三步（阻塞执行，出现异常会抛出）
+    """
+    # 第 1 步
+    common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish1.png'], device)
+    # 第 2 步（带置信度参数）
+    mayFind = common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/yu_hun_tmp_jie_mian.png'], device, 0.64)
+    # 第 3 步
+    common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish2.png'], device)
+
+    if mayFind:
+        common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish2.png'], device)
+
+
 def click_chenggong():
+    #最新并发
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(run_sequence, 'host')
+        executor.submit(run_sequence, 'my')
     # success = common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish1.png'], host)
     # success = common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish1.png'], my)
     # success = common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish2.png'], host)
     # success = common.myadb.find_and_click_adb_many_picture(['./picture/yuhun/finish2.png'], my)
     # 前两行并发执行
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/finish1.png'], host)
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/yu_hun_tmp_jie_mian.png'], host, 0.64)
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/finish2.png'], host)
-
-    # 后两行并发执行
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/finish1.png'], my)
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/yu_hun_tmp_jie_mian.png'], my, 0.64)
-        executor.submit(common.myadb.find_and_click_adb_many_picture,
-                        ['./picture/yuhun/finish2.png'], my)
+    # with ThreadPoolExecutor(max_workers=1) as executor:
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/finish1.png'], host)
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/yu_hun_tmp_jie_mian.png'], host, 0.64)
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/finish2.png'], host)
+    #
+    # # 后两行并发执行
+    # with ThreadPoolExecutor(max_workers=1) as executor:
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/finish1.png'], my)
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/yu_hun_tmp_jie_mian.png'], my, 0.64)
+    #     executor.submit(common.myadb.find_and_click_adb_many_picture,
+    #                     ['./picture/yuhun/finish2.png'], my)
 
 
 tn = time.time()
